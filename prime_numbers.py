908bec8e789cd7179d17997b9156bc5f1e1bdc4e
Ifra Zaib <ifraa.zaib@gmail.com>
2024-06-29 18:42:48 +0500

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,7 +1,4 @@
-class PrimeNumber:
-    """class to check whether a number is prime and to find prime numbers in a range"""
-
-    def prime_numbers_check(self, num):
+def prime_numbers_check(self, num):
         """check whether a given number is prime or not"""
         if num == 1:  # as 1 is not a prime number
             print("Number is not a Prime")
@@ -18,7 +15,7 @@ class PrimeNumber:
         else:
             print("Wrong input")
 
-    def prime_by_loop(self, start, end):
+def prime_by_loop(self, start, end):
         """check and print prime numbers in a given range"""
         for num in range(start, end + 1):
             if num > 1:
@@ -32,9 +29,8 @@ class PrimeNumber:
 
 
 num = int(input("Enter a number to check either prime or not : "))  # create an instance of PrimeNumber class
-obj = PrimeNumber()
-obj.prime_numbers_check(num)  # call the method to check if the number is prime
+prime_numbers_check(num)  # call the method to check if the number is prime
 
 start = int(input("Enter your start number to check in a range: "))
 end = int(input("Enter your end number to check a number in a range: "))
-obj.prime_by_loop(start, end)  # call the method to print prime numbers in the given range
+prime_by_loop(start, end)  # call the method to print prime numbers in the given range
