7df0e3ce913091bfc0a8ac311381f78a6560dd9c
Ifra Zaib <ifraa.zaib@gmail.com>
2024-07-01 18:34:28 +0500

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,29 +1,22 @@
 def is_prime(num):
     if num <= 1: # 1 is not a prime number
-        print("Not a prime number")
-        return
-    for i in range(2, num): # loop to check range
-        if num % i == 0: # check condition if divided then prime
-            print("Not a prime number")
-            return
-    print("Prime number")
+        return False
+    for div in range(2, num // 2 + 1): # loop to check range
+        if num % div == 0: # check condition if divided then prime
+            return False
+    return True
 
 
 def print_primes_in_loop(num):
-    if num <= 1:
-        print("No prime numbers")
-        return
     for n in range(2, num + 1): # outer loop 
-        is_prime = True
-        for i in range(2, n): # inner loop to check the outer loop number is prime or not
-            if n % i == 0:
-                is_prime = False
-                break
-        if is_prime:
+       if is_prime(n):  # check if n is prime
             print(n, end=" ")
 
 
 num = int(input("Enter a number: "))
-is_prime(num) # function calling
-print("Prime numbers in a loop:")
+is_prime_check = is_prime(num)  # function calling
+if is_prime_check:
+    print(f"{num} is a Prime Number")
+else:
+    print(f"{num} is Not a Prime Number")
 print_primes_in_loop(num)
